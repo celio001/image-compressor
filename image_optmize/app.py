c40ecd909e0587a63fe3f3d30506a95887e78cce
celio001 <pessoalcelio01@gmail.com>
2025-03-01 18:55:45 -0300

    feat: compressor webp

--- image_optmize/app.py
+++ image_optmize/app.py
@@ -1,44 +1,47 @@
 from abc import ABC, abstractmethod
 from PIL import ImageFile, Image
 from pathlib import Path
+from io import BytesIO
 from exceptions import NotImage
+
 class ImageOptimizerTypesStrategy(ABC):
 
     @abstractmethod
-    def optmize(self, img: ImageFile, output_path: str, quality: int) -> None:
+    def optmize(self, img: ImageFile, quality: int) -> None:
         pass
 
 class WebpPilOptimizer(ImageOptimizerTypesStrategy):
 
-    def optmize(self, img, output_path, quality):
-        print('Estou otimizando')
-
-class WebpCVOptimizer(ImageOptimizerTypesStrategy):
-    def optmize(self, img, output_path, quality):
-        print('Estou otimizando com opencv')
+    def optmize(self, img: ImageFile, quality: int):
+        output_path = BytesIO()
+        img.save(output_path, 'webp', optimize=True, quality=quality)
+        return output_path
 
 #Strategy
 class ImageOptimizer:
-    def __init__(self, inpuy_path: str, ouytput_path: str, compression: int, optimizer: ImageOptimizerTypesStrategy):
+    def __init__(self, inpuy_path: str, compression: int, optimizer: ImageOptimizerTypesStrategy):
         self.inpuy_path = inpuy_path
-        self.ouytput_path = ouytput_path
 
-        if not Path(self.ouytput_path).exists():
+        if not Path(self.output_path).exists():
             raise ModuleNotFoundError()
         
         self.compression = compression 
         self.optimizer = optimizer
+        self.img = self._open_image()
     
     def optimize(self):
-        self.optimizer.optmize('', '', '')
+        return self.optimizer.optmize(img=self.img, quality=self.quality)
 
     def _open_image(self):
         try:
             img = Image.open(self.inpuy_path)
             img.verify()
+            return Image.open(self.inpuy_path)
         except:
             raise NotImage(self.inpuy_path)
-        
 
-x = ImageOptimizer('', '', 0, WebpCVOptimizer())
-x.optimize()
+    @property
+    def quality(self):
+        return 1 if (100 - self.compression) == 0 else (100 - self.compression)
+    
+    
